8e20f512e751b8d77641b78f807ec6fcead3d42a
Oleg Smirnov <smirnovob1417@mail.ru>
2023-03-27 19:27:20 +0300

    final

--- taskNumber2.java
+++ taskNumber2.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
@@ -9,39 +10,36 @@ import java.util.logging.SimpleFormatter;
 
 public class taskNumber2 {
 
-    public static void main(String[] args) throws IOException {
-        int[] array = { 6, 4, 2, 7, 12, 4, 45, 1 };
+    public static final Logger logger = Logger.getLogger(taskNumber2.class.getName());
 
-        garden(array);
-    }
-
-    public static int[] garden(int array[]) throws IOException {
-        Logger logger = Logger.getLogger(taskNumber2.class.getName());
-        FileHandler fHandler = new FileHandler("taskNumber2.txt");
+    public static void main(String[] args) throws Exception {
+        Handler fHandler = new FileHandler("taskNumber2.txt");
         SimpleFormatter sFormat = new SimpleFormatter();
         fHandler.setFormatter(sFormat);
         logger.addHandler(fHandler);
+
+        int[] c = { 6, 4, 2, 7, 12, 4, 45, 1 };
+        garden(c);
+    }
+
+    public static void garden(int[] array) throws IOException {
+
         logger.info(Arrays.toString(array));
-        
-        int[] b = array;
-        int count = 0;
-        int p = 0;
-        for (int i = 1; i < b.length; i++) {
-            p = 0;
-            if (b[i] < b[i - 1]) {
-                p = b[i];
-                b[i] = b[i - 1];
-                b[i - 1] = p;
-                count++;
+
+        int[] newArray = array;
+        boolean temp = false;
+        int temporalCh = 0;
+        for (int i = 1; i < newArray.length; i++) {
+            temporalCh = 0;
+            if (newArray[i] < newArray[i - 1]) {
+                temporalCh = newArray[i];
+                newArray[i] = newArray[i - 1];
+                newArray[i - 1] = temporalCh;
+                temp = true;
             }
-        }      
-        for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i] + " ");
         }
-        System.out.println();
-        if (count > 0) {
-            garden(b);
+        if (temp) {
+            garden(newArray);
         }
-        return b;
     }
 }
